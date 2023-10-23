5a53b2e92509f654b1066736f9fe1667a388ef76
partychad <partychad@protonmail.com>
2023-10-23 22:37:27 +0200

    "Title: Refactor main function into multiple subcommands\n\nDescription: \nThe main.rs file has been refactored to include multiple subcommands such as \'s\' for status display, \'d\' for displaying the generated commit message without committing, \'config\' for displaying configuration parameters, \'c\' for commit, and \'p\' for push.\n\nEach subcommand has its dedicated function such as display_status(), display_commit_message(), display_config(), commit(), and push(). Error handling has been improved to prevent committing when the commit message is empty."

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,93 @@
 
+//
+// mod gpt_api;
+// mod commands;
+// fn main() {
+//     use gpt_api::CommitMessageGenerator;
+//     use commands::Commands;
+//     let git_diff = Commands::new("git".to_string(), vec!["diff".to_string()]);
+//     let git_status = Commands::new("git".to_string(), vec!["status".to_string()]);
+//     let git_add = Commands::new("git".to_string(), vec!["add".to_string(), ".".to_string()]);
+//     let git_push = Commands::new("git".to_string(), vec![ "push".to_string(), "--force-with-lease".to_string()]);
+//
+//
+//     let generator = CommitMessageGenerator::new(
+//         "https://api.openai.com/v1/chat/completions",
+//         "gpt-4",
+//         "Based on the following git diff and git status output, suggest a formatted and structured but succinct commit message\n",
+//         "GPT_API_KEY"
+//     );
+//
+//     let commit_msg = generator.generate_commit_message(&git_diff.call(), &git_status.call());
+//     let final_msg = match commit_msg {
+//         Ok(msg) => {
+//             println!("{}", msg);
+//             msg
+//         },
+//         Err(err) => {
+//             eprintln!("Error encountered: {}", err);
+//             return
+//         },
+//     };
+//
+//     let git_commit = Commands::new("git".to_string(), vec!["commit".to_string(), "-m".to_string(), final_msg]);
+//
+//     git_add.call();
+//     git_commit.call();
+//     git_push.call();
+//
+//
+// }
 
 mod gpt_api;
 mod commands;
+use clap::{App, SubCommand};
+use gpt_api::CommitMessageGenerator;
+use commands::Commands;
 fn main() {
-    use gpt_api::CommitMessageGenerator;
-    use commands::Commands;
+    let matches = App::new("Git GPT Helper")
+        .version("1.0")
+        .author("Your Name <partychad@protonmail.com>")
+        .about("Automates git commit messages using GPT suggestions.")
+        .subcommand(SubCommand::with_name("s")
+            .about("Displays git status"))
+        .subcommand(SubCommand::with_name("d")
+            .about("Display the generated commit message without committing"))
+        .subcommand(SubCommand::with_name("config")
+            .about("Displays the configuration parameters"))
+        .subcommand(SubCommand::with_name("c")
+            .about("Commits the changes"))
+        .subcommand(SubCommand::with_name("p")
+            .about("Pushes the changes (add + commit + push)"))
+        .get_matches();
+
+    // Check which subcommand (if any) was used
+    match matches.subcommand_name() {
+        Some("s") => display_status(),
+        Some("d") => { display_commit_message(); },
+        Some("config") => display_config(),
+        Some("c") => commit(),
+        Some("p") => push(),
+        None => println!("No subcommand was used"),
+        _ => unreachable!(),  // If someone added a subcommand but didn't add a case here
+    }
+}
+
+fn display_status() {
+    let git_status = Commands::new("git".to_string(), vec!["status".to_string()]);
+    let  status = git_status.call();
+    println!("Status {}", status);
+}
+
+fn display_commit_message() -> String {
+    let generator = CommitMessageGenerator::new(
+    "https://api.openai.com/v1/chat/completions",
+    "gpt-4",
+    "Based on the following git diff and git status output, suggest a formatted and structured but succinct commit message\n",
+    "GPT_API_KEY"
+    );
     let git_diff = Commands::new("git".to_string(), vec!["diff".to_string()]);
     let git_status = Commands::new("git".to_string(), vec!["status".to_string()]);
-    let git_add = Commands::new("git".to_string(), vec!["add".to_string(), ".".to_string()]);
-    let git_push = Commands::new("git".to_string(), vec![ "push".to_string(), "--force-with-lease".to_string()]);
-
-
-    let generator = CommitMessageGenerator::new(
-        "https://api.openai.com/v1/chat/completions",
-        "gpt-4",
-        "Based on the following git diff and git status output, suggest a formatted and structured but succinct commit message\n",
-        "GPT_API_KEY"
-    );
-
     let commit_msg = generator.generate_commit_message(&git_diff.call(), &git_status.call());
     let final_msg = match commit_msg {
         Ok(msg) => {
@@ -26,16 +96,34 @@ fn main() {
         },
         Err(err) => {
             eprintln!("Error encountered: {}", err);
-            return
+            return String::new()
         },
     };
-
-    let git_commit = Commands::new("git".to_string(), vec!["commit".to_string(), "-m".to_string(), final_msg]);
-
-    git_add.call();
-    git_commit.call();
-    git_push.call();
-
+    println!("Status {}", final_msg);
+    final_msg
 
 }
+
+fn display_config() {
+    // Your code to display the configuration parameters
+}
+
+fn commit() {
+    let commit_msg = display_commit_message();
+    if !commit_msg.is_empty() {
+        let git_commit = Commands::new("git".to_string(), vec!["commit".to_string(), "-m".to_string(), commit_msg]);
+        let git_add = Commands::new("git".to_string(), vec!["add".to_string(), ".".to_string()]);
+        git_add.call();
+        git_commit.call();
+     } else {
+        eprintln!("Error: Commit message is empty!");
+    }
+}
+
+fn push() {
+    commit();
+    let git_push = Commands::new("git".to_string(), vec![ "push".to_string(), "--force-with-lease".to_string()]);
+    git_push.call();
+}
+
 
